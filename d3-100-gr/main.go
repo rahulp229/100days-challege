aa3f1857260114b5727fea2cacb7a919d9992835
rahulp229 <rahulp229@gmail.com>
2025-01-16 20:13:51 +0530

    ludo game

---
+++ d3-100-gr/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	wg := sync.WaitGroup{}
+	ch := make(chan bool)
+	wg.Add(10)
+	for i := 1; i <= 10; i++ {
+		go task(i, ch, &wg)
+	}
+	go func() {
+		wg.Wait()
+
+		close(ch)
+	}()
+	go func() {
+		for signal := range ch {
+			fmt.Println(signal)
+			if signal {
+				// Handle the signal here
+				ch <- false
+			}
+		}
+	}()
+
+	time.Sleep(time.Second * 2)
+}
+
+func task(i int, ch chan bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(i)
+	ch <- true
+}
